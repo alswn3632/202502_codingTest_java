f83c8294cc1d9b100171ae69b74e59286a738d92
alswn3632 <als3632@naver.com>
2025-02-10 20:39:31 +0900

    020 문제풀이

---
+++ src/programmers/Problem020.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Problem020 {
+    public int solution(String s) {
+
+        String[] arr = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine"};
+
+        // 영단어를 통해 숫자로 변환
+        for (int i=0; i<arr.length; i++) {
+            s = s.replaceAll(arr[i], String.valueOf(i));
+        }
+
+        // 문자열을 정수로 변환해 제출
+        return Integer.parseInt(s);
+    }
+}
