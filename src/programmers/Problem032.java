62f4b2ec9ef66772f3d3d2d4b2e4e25e78238c81
alswn3632 <als3632@naver.com>
2025-02-26 23:08:23 +0900

    032 문제풀이

---
+++ src/programmers/Problem032.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class Problem032 {
+    //https://school.programmers.co.kr/learn/courses/30/lessons/12973
+    //2017 팁스타운
+
+    public int solution(String s){
+        int answer = -1;
+
+        // 문자열 길이가 홀수라면 제거 실패
+        if(s.length()%2 == 1){ return 0; }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        answer = stack.isEmpty() ? 1 : 0;
+
+        return answer;
+    }
+}
