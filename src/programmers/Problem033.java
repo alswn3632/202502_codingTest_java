662348be54deba4574155db04ca5a808bf25dbf2
alswn3632 <als3632@naver.com>
2025-02-28 22:11:56 +0900

    033 문제풀이

---
+++ src/programmers/Problem033.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Problem033 {
+    //https://school.programmers.co.kr/learn/courses/30/lessons/42885
+    //탐욕법(Greedy)
+
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
