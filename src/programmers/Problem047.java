e41aea18cf6a282b1a9193d1304139d145b7a305
alswn3632 <als3632@naver.com>
2025-03-19 19:13:57 +0900

    047 문제풀이

---
+++ src/programmers/Problem047.java
@@ -0,0 +1,37 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem047 {
+    //https://school.programmers.co.kr/learn/courses/30/lessons/42586
+    //스택/큐
+
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+
+        List<Integer> list = new ArrayList<>();
+        int n = progresses.length;
+
+        for (int i=0; i<n; i++) {
+            list.add((int)Math.ceil((100.0 - progresses[i])/speeds[i]));
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        while (!list.isEmpty()) {
+            int maxDay = list.get(0);
+            int count = 0;
+
+            while (!list.isEmpty() && list.get(0) <= maxDay) {
+                list.remove(0);
+                count++;
+            }
+
+            result.add(count);
+        }
+
+        answer = result.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
