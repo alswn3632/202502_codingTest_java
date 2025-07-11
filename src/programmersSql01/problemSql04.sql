e59f155299f2a13a789b988bc2cee04e90b63612
alswn3632 <als3632@naver.com>
2025-07-11 21:54:50 +0900

    Sql 04 문제풀이

---
+++ src/programmersSql01/problemSql04.sql
@@ -0,0 +1,9 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/59411
+--오랜 기간 보호한 동물(2)
+
+SELECT
+    AO.ANIMAL_ID,
+    AO.NAME
+FROM ANIMAL_OUTS AO
+    INNER JOIN ANIMAL_INS AI ON AO.ANIMAL_ID = AI.ANIMAL_ID
+ORDER BY AO.DATETIME - AI.DATETIME DESC LIMIT 2
